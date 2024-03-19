ade48180635f8ededc8b510dbe0ef9daa16a5c87
Sandeex <kirillkopaev@gmail.com>
2024-03-19 15:29:15 +0300

    refactoring

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,5 +1,6 @@
-#ifndef OP_VECTOR_H
-#define OP_VECTOR_H
+#ifndef INC_VECTOR_H
+#define INC_VECTOR_H
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -8,15 +9,45 @@ typedef struct vector {
     size_t size; // размер вектора
     size_t capacity; // вместимость вектора
 } vector;
-vector createVector(size_t n);
-void* validateMemory(void* memory);
-void reserve(vector *v, size_t newCapacity);
-void clear(vector *v);
-void shrinkToFit(vector *v);
-void deleteVector(vector *v);
-bool isEmpty(vector *v);
-bool isFull(vector *v);
-int getVectorValue(vector *v, size_t i);
-void pushBack(vector *v, int x);
 
-#endif //OP_VECTOR_H
+
+// возвращает структуру-дескриптор вектор из n значений
+vector createVector(size_t capacity);
+
+// изменяет количество памяти, выделенное под хранение элементов вектора
+void reserve(vector *vec, size_t newCapacity);
+
+// удаляет элементы из контейнера, но не освобождает выделенную память
+void clear(vector *vec);
+
+// освобождает память, выделенную под неиспользуемые элементы
+void shrinkToFit(vector *vec);
+
+// освобождает память, выделенную вектору
+void deleteVector(vector *vec);
+
+// функция проверки на то, является ли вектор пустым
+bool isEmpty(vector *vec);
+
+// функция проверки на то, является ли вектор полным
+bool isFull(vector *vec);
+
+// возвращает i-ый элемент вектора vec
+int getVectorValue(vector *vec, size_t i);
+
+// добавляет элемент x в конец вектора v
+void pushBack(vector *vec, int x);
+
+// удаляет последний элемент из вектора
+void popBack(vector *vec);
+
+// возвращает указатель на index-ый элемент вектора
+int* atVector(vector *vec, size_t index);
+
+// возвращает указатель на последний элемент вектора
+int* back(vector *v);
+
+// возвращает указатель на нулевой элемент вектора
+int* front(vector *v);
+
+#endif
