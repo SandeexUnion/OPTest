17486312ab54ad625c4951ba6f9529b58598dda8
Sandeex <kirillkopaev@gmail.com>
2024-05-27 15:29:14 +0300

    first commit

---
+++ OP/lab20.c
@@ -0,0 +1,5 @@
+//
+// Created by Sandex Union csgorum on 27.05.2024.
+//
+
+#include "lab20.h"
---
+++ OP/lab20.h
@@ -0,0 +1,8 @@
+//
+// Created by Sandex Union csgorum on 27.05.2024.
+//
+
+#ifndef OP_LAB20_H
+#define OP_LAB20_H
+
+#endif //OP_LAB20_H
